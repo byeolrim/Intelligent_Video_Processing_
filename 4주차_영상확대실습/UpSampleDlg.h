895bcb8778ab134b16ee385248c5671922699221
byeolrim <128365506+byeolrim@users.noreply.github.com>
2025-04-03 14:52:33 +0900

    Create UpSampleDlg.h

---
+++ 4주차_영상확대실습/UpSampleDlg.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+// CUpSampleDlg 클래스 선언
+class CUpSampleDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CUpSampleDlg)
+
+public:
+	CUpSampleDlg(CWnd* pParent = nullptr);   // 생성자
+	virtual ~CUpSampleDlg(); // 소멸자
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX); // DDX/DDV 지원
+
+	DECLARE_MESSAGE_MAP()
+
+private:
+	int m_UpSampleRate; // 샘플링 비율을 저장하는 변수
+
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 }; // 대화 상자 ID 정의
+#endif
+};
